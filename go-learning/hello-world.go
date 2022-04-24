7a8b4e71d39ee7ea0a77d2eaadb7549ee9c32f9c
chedeane <che.deane@gmail.com>
2022-04-24 22:53:15 +0000

    added first for loop

--- go-learning/hello-world.go
+++ go-learning/hello-world.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	for kounter := 0; kounter <= 5; kounter++ {
+		fmt.Printf("This is the kounter value %v\n", kounter)
+	}
+	fmt.Println("bye world")
+
 }
