80a81e5da42ff7b4c863106c905ea7f698f435a3
chedeane <che.deane@gmail.com>
2022-05-12 21:48:20 +0000

    closure work

---
+++ go-learning/closure-go.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func inSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := inSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+}
