1e28430f2f67065646933bd968a2795f1f7729a3
chedeane <che.deane@gmail.com>
2022-07-01 14:35:48 +0000

    pratice continue

---
+++ go-learning/textTemplates-go.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+	t1 := template.New("t1")
+
+	t1, err := t1.Parse("Value is {{.}}\n")
+	if err != nil {
+		panic(err)
+	}
+
+	t1 = template.Must(t1.Parse("Value: {{.}}\n"))
+
+	t1.Execute(os.Stdout, "some text")
+	t1.Execute(os.Stdout, 5)
+	t1.Execute(os.Stdout, []string{
+		"Go",
+		"Rust",
+		"C++",
+		"C#",
+	})
+
+	Create := func(name, t string) *template.Template {
+		return template.Must(template.New(name).Parse(t))
+	}
+
+	t2 := Create("t2", "Name: {{.Name}}\n")
+
+	t2.Execute(os.Stdout, struct {
+		Name string
+	}{"Jane Doe"})
+
+	t2.Execute(os.Stdout, map[string]string{
+		"Name": "Mickey Mouse",
+	})
+
+}
