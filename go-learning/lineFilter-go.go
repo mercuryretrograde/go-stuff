d30690fe6b0d7afc4293f438067cf28df7295b97
chedeane <che.deane@gmail.com>
2022-07-22 17:33:41 +0000

    end practice

---
+++ go-learning/lineFilter-go.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+	fmt.Println()
+	log.Print("This is our first log message in Go.")
+
+}
